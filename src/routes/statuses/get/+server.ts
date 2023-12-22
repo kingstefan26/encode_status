b205d718c1475b215598ed269ee9b25c50e92c08
kingstefan26 <70776766+kingstefan26@users.noreply.github.com>
2023-12-22 05:57:26 +0100

    show that statuses are idle, hide old ones

--- src/routes/statuses/get/+server.ts
+++ src/routes/statuses/get/+server.ts
@@ -21,7 +21,21 @@ export const GET = (async ({platform, request}) => {
     // }
 
     // @ts-ignore
-    const data = await platform.env.MAIN.get('entries', {type: 'json'}) || []
+    let data = await platform.env.MAIN.get('entries', {type: 'json'}) || []
+
+    // if last update was 2min + change ago, set phase to "Idle"
+    // if for 2 days+ last online, delete the status from the list
+
+    data = data.map((entry: any) => {
+        if (Date.now() - entry.lastupdate > 2 * 60 * 1000) {
+            entry.phase = 'Idle'
+        }
+        return entry
+
+    })
+
+    data = data.filter((entry: any) => Date.now() - entry.lastupdate < 2 * 24 * 60 * 60 * 1000)
+
 
     return new Response(JSON.stringify(data))
 
