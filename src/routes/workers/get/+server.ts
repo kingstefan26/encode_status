127d2158342a6e9e4cafc743aa6d69e5dc62fb9e
kingstefan26 <70776766+kingstefan26@users.noreply.github.com>
2023-12-22 06:06:14 +0100

    readjust to 10mins

--- src/routes/workers/get/+server.ts
+++ src/routes/workers/get/+server.ts
@@ -3,10 +3,10 @@ import type {RequestHandler} from "@sveltejs/kit/src/exports/public";
 export const GET = (async ({platform, request}) => {
     const data = await (await platform?.env.MAIN.get('workers', {type: 'json'}) || [])
 
-    // if the last online date was 2min + change ago, set status to offline.
+    // if the last online date was 10min + change ago, set status to offline.
     // if for 2 days+ last online, delete the worker from the list
     data.forEach((worker: any) => {
-        if (Date.now() - worker.lastupdate > 2 * 60 * 1000) {
+        if (Date.now() - worker.lastupdate > 10 * 60 * 1000) {
             worker.status = 'offline'
         }
         if (Date.now() - worker.lastupdate > 2 * 24 * 60 * 60 * 1000) {
