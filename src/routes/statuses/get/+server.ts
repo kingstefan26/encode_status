a6222043ded9b10814b5c87aa8c806738779950f
kingstefan26 <70776766+kingstefan26@users.noreply.github.com>
2023-12-23 05:32:55 +0100

    me when

--- src/routes/statuses/get/+server.ts
+++ src/routes/statuses/get/+server.ts
@@ -3,40 +3,39 @@ import type {RequestHandler} from "@sveltejs/kit/src/exports/public";
 
 export const GET = (async ({platform, request}) => {
 
-    // let cache = await platform?.caches.default || null
-    //
-    // let response = await cache?.match(request)
-    //
-    // if (!response) {
-    //     const data = await (await platform?.env.MAIN.get('entries', {type: 'json'}) || [])
+    let cache = platform?.caches.default || null
 
-        // response = new Response(JSON.stringify(data));
+    let response = await cache?.match(request)
 
-        // if (cache){
-        //     response.headers.append('Cache-Control', 'max-age=60, s-maxage=60')
-        //
-        //     platform?.context.waitUntil(cache.put('get', response.clone()))
-        // }
+    if (!response) {
+        // @ts-ignore
+        let data = await platform.env.MAIN.get('entries', {type: 'json'}) || []
 
-    // }
+        // if last update was 30min + change ago, set phase to "Idle"
+        data = data.map((entry: any) => {
+            if (Date.now() - entry.lastupdate > 30 * 60 * 1000 && entry.phase !== 'Done') {
+                entry.phase = 'Idle'
+            }
+            return entry
 
-    // @ts-ignore
-    let data = await platform.env.MAIN.get('entries', {type: 'json'}) || []
+        })
 
-    // if last update was 2min + change ago, set phase to "Idle"
-    // if for 2 days+ last online, delete the status from the list
+        // if for 2 days+ last online, delete the status from the list
+        data = data.filter((entry: any) => Date.now() - entry.lastupdate < 2 * 24 * 60 * 60 * 1000)
 
-    data = data.map((entry: any) => {
-        if (Date.now() - entry.lastupdate > 2 * 60 * 1000 && entry.phase !== 'Done') {
-            entry.phase = 'Idle'
+
+        let response = new Response(JSON.stringify(data));
+
+        if (cache){
+            response.headers.append('Cache-Control', 'max-age=240, s-maxage=240')
+
+            platform?.context.waitUntil(cache.put('get', response.clone()))
         }
-        return entry
 
-    })
-
-    data = data.filter((entry: any) => Date.now() - entry.lastupdate < 2 * 24 * 60 * 60 * 1000)
+        return response
+    }
 
 
-    return new Response(JSON.stringify(data))
+    return response
 
 }) satisfies RequestHandler
--- src/routes/workers/get/+server.ts
+++ src/routes/workers/get/+server.ts
@@ -1,12 +1,21 @@
 import type {RequestHandler} from "@sveltejs/kit/src/exports/public";
 
 export const GET = (async ({platform, request}) => {
+
+    let cache = platform?.caches.default || null
+
+    let response = await cache?.match(request)
+
+    if (response) {
+        return response
+    }
+
     const data = await (await platform?.env.MAIN.get('workers', {type: 'json'}) || [])
 
-    // if the last online date was 10min + change ago, set status to offline.
+    // if the last online date was 30min + change ago, set status to offline.
     // if for 2 days+ last online, delete the worker from the list
     data.forEach((worker: any) => {
-        if (Date.now() - worker.lastupdate > 10 * 60 * 1000) {
+        if (Date.now() - worker.lastupdate > 30 * 60 * 1000) {
             worker.status = 'offline'
         }
         if (Date.now() - worker.lastupdate > 2 * 24 * 60 * 60 * 1000) {
@@ -15,5 +24,15 @@ export const GET = (async ({platform, request}) => {
         }
     })
 
-    return new Response(JSON.stringify(data))
+    response = new Response(JSON.stringify(data));
+
+    if (cache) {
+        response.headers.append('Cache-Control', 'max-age=240, s-maxage=240')
+
+        platform?.context.waitUntil(cache.put('get', response.clone()))
+    }
+
+    return response
+
+
 }) satisfies RequestHandler
